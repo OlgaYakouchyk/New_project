ee7ffedadf6988524ed493e2f4a548379926ade6
OlgaYakouchyk <109695976+OlgaYakouchyk@users.noreply.github.com>
2023-08-23 20:43:45 -0400

    new project

---
+++ script.js
@@ -0,0 +1,13 @@
+const newArray = [23, 97, 15, "Hello", "hi", "one"];
+
+for (let i = 0; i < newArray.length; i++) {
+  if (newArray[i] !== String(newArray[i])) {
+    console.log(newArray[i]);
+  }else{
+    console.log('jhfsbcn');
+  }
+}
+const a =true;
+if(a){
+console.log('false');
+}
